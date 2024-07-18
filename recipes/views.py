2948374d8e468933a02fab9faece3d7911a181ce
Romario Rodrigues <romariobdc2011@gmail.com>
2024-07-18 11:39:16 -0300

    Removido contato e sobre

--- recipes/views.py
+++ recipes/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
 
 # Create your views here.
 
@@ -10,8 +10,3 @@ recipes/templaes/home.html.
 def home(request):
     return render(request, 'recipes/home.html')
 
-def sobre(request):
-    return HttpResponse('sobre')
-
-def contato(request):
-    return HttpResponse('contato')
